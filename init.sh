1241177a991ac4a7b0dcf5e11db2ae787dd0b6fb
Zavarka Team <support@zavarka.team>
2022-05-09 17:30:54 +0300

    Modify init

--- init.sh
+++ init.sh
@@ -3,3 +3,11 @@ sudo rm -rf /usr/share/keyrings/docker-archive-keyring.gpg; curl -fsSL https://d
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null; \
 sudo apt-get update -y; sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin;
+ssh-keygen -t ed25519 -C "atlantm@YaCloud" -f ~/.ssh/id_ed25519 -P '';
+sudo usermod -a -G www-data atlantm;
+sudo mkdir /srv/www/; sudo mkdir /srv/docker/; sudo mkdir /srv/config/;
+sudo chown atlantm:www-data /srv/config; sudo chown atlantm:www-data /srv/www;
+
+echo $(cat ~/.ssh/id_ed25519.pub);
+read -p "Add this key to read-only repo and press Enter...";
+git clone git@bitbucket.org:zavarkateam/atlantm-config.git /srv/config;
