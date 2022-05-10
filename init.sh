5960c13d163cc2ad771ec8c2f083bd40bd98bb4c
Zavarka Team <support@zavarka.team>
2022-05-10 15:08:51 +0300

    Add vsftpd container
    Fix php-fpm settings

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo apt update -y; sudo apt upgrade -y; sudo apt install -y mc git zsh vsftpd docker ca-certificates curl lynx gnupg lsb-release; \
+sudo apt update -y; sudo apt upgrade -y; sudo apt install -y mc git zsh docker ca-certificates curl lynx gnupg lsb-release; \
 sudo rm -rf /usr/share/keyrings/docker-archive-keyring.gpg; curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg; \
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null; \
