039470740212fec5a9d183d0f1b79212a607a133
Zavarka Team <support@zavarka.team>
2022-05-12 19:44:46 +0300

    Add phpmyadmin container

---
+++ phpmyadmin/config.user.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+$cfg['Export']['method'] = 'custom';
+$cfg['Export']['compression'] = 'zip';
+$cfg['Export']['lock_tables'] = true;
+$cfg['Export']['charset'] = 'utf-8';
+$cfg['Export']['sql_drop_table'] = true;
+$cfg['Export']['sql_type'] = 'REPLACE';
+$cfg['Console']['StartHistory'] = true;
+$cfg['Console']['Mode'] = 'collapse';
+$cfg['Server']['hide_db'] = '';
+$cfg['Server']['only_db'] = '';
+$cfg['lang'] = 'ru';
