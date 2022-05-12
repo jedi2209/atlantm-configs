e77d55741d11061f8e70e03908b228038f2fdee4
Zavarka Team <support@zavarka.team>
2022-05-12 19:51:13 +0300

    Add SSL to phpmyadmin connect

--- phpmyadmin/config.user.inc.php
+++ phpmyadmin/config.user.inc.php
@@ -11,3 +11,16 @@ $cfg['Console']['Mode'] = 'collapse';
 $cfg['Server']['hide_db'] = '';
 $cfg['Server']['only_db'] = '';
 $cfg['lang'] = 'ru';
+
+// IP address of your instance
+$cfg['Servers'][$i]['host'] = 'rc1b-6hjqm8by6fiennpe.mdb.yandexcloud.net';
+// Use SSL for connection
+$cfg['Servers'][$i]['ssl'] = true;
+// // Client secret key
+// $cfg['Servers'][$i]['ssl_key'] = '../client-key.pem';
+// // Client certificate
+// $cfg['Servers'][$i]['ssl_cert'] = '../client-cert.pem';
+// Server certification authority
+$cfg['Servers'][$i]['ssl_ca'] = '/mysql/root.crt';
+// Disable SSL verification (see above note)
+$cfg['Servers'][$i]['ssl_verify'] = false;
