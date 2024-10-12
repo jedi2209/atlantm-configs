0b5ad840940fc53a99f3d4a38449972f7d1466f0
Zavarka Team <support@zavarka.team>
2024-10-12 23:01:05 +0000

    Update cloud config

--- docker.init.sh
+++ docker.init.sh
@@ -5,4 +5,38 @@ if ! command -v docker-compose &> /dev/null; then
   exit 1
 fi
 
+# Array of directories containing Nginx configuration files
+CONFIG_DIRS=(
+  "./nginx/vhost"
+  "./nginx/params"
+)
+
+# Remove .off suffix from all files
+for dir in "${CONFIG_DIRS[@]}"; do
+  for file in "$dir"/*.off; do
+    if [ -f "$file" ]; then
+      mv "$file" "${file%.off}"
+      printf "\tRevert\t$file\t\t\t=>\t\t${file%.off}\r\n"
+    fi
+  done
+done
+
+printf "\r\n\r\n";
+
+# Iterate over each directory in the array
+for dir in "${CONFIG_DIRS[@]}"; do
+  # Iterate over all .conf files in the directory
+  for file in "$dir"/*.conf; do
+    # Extract the root parameter
+    root_dir=$(grep -oP 'root\s+\K\S+' "$file")
+
+    # Check if the root directory exists
+    if [ ! -d "$root_dir" ]; then
+      # Rename the file with a .off suffix
+      mv "$file" "$file.off"
+      printf "\tRenamed\t$file\t\t\t=>\t\t$file.off\r\n"
+    fi
+  done
+done
+
 cd /srv/config && docker-compose stop && docker-compose down && docker-compose up --build --detach --force-recreate;
--- init.sh
+++ init.sh
@@ -6,16 +6,15 @@ WWW_DIR='/www';
 DOCKER_DIR='/docker';
 CONFIG_DIR='/config';
 
-sudo apt update -y; sudo apt upgrade -y; sudo apt install -y mc git zsh ncdu docker mysql-client ca-certificates curl lynx gnupg lsb-release screen s3cmd software-properties-common; \
+sudo apt update -y; sudo apt upgrade -y; sudo apt install -y mc git zsh ncdu docker mysql-client ca-certificates curl gpg lynx gnupg lsb-release screen s3cmd software-properties-common; \
 sudo rm -rf /usr/share/keyrings/docker-archive-keyring.gpg; curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg; \
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -; sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"; \
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg; gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint; \
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list; \
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null; \
-sudo apt-get update -y; sudo apt-get install -y docker-ce docker-ce-cli terraform containerd.io docker-compose-plugin docker-compose; \
-sudo ufw allow openssh; sudo ufw allow ftp; sudo service ufw restart; \
+sudo apt-get update -y; sudo ufw allow openssh; sudo ufw allow ftp; sudo service ufw restart; \
 ssh-keygen -t ed25519 -C "atlantm@YaCloud" -f ~/.ssh/id_ed25519 -P ''; \
 sudo usermod -a -G www-data atlantm; \
-terraform -install-autocomplete; \
 sudo mkdir ${DEFAULT_DIR}${WWW_DIR}; sudo mkdir ${DEFAULT_DIR}${DOCKER_DIR}; sudo mkdir ${DEFAULT_DIR}${CONFIG_DIR}; \
 sudo chown atlantm:www-data ${DEFAULT_DIR}${CONFIG_DIR}; sudo chown atlantm:www-data ${DEFAULT_DIR}${WWW_DIR}; \
 echo $(cat ~/.ssh/id_ed25519.pub); \
@@ -26,6 +25,5 @@ mkdir ~/.mysql && \
 wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" -O ~/.mysql/root.crt && \
 chmod 0600 ~/.mysql/root.crt
 cd /srv/config; docker-compose up --no-start; docker-compose restart && crontab cron/crontab;
-cd ~; curl -sSL https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash;
 
 sudo mount -t virtiofs api ${DEFAULT_DIR}${WWW_DIR}/xxx.com/public_html/mnt_files;
